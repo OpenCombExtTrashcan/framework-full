1ea114ef718d5e41edae0a2d1d774d8fee71c92d
alee <alee@fb4ae8c7-ff60-4932-ad58-89b0e700feac>
2011-04-27 10:43:23 +0000

    git-svn-id: http://svn.jecat.cn/framework/0.6/trunk/test@2442 fb4ae8c7-ff60-4932-ad58-89b0e700feac

--- integration/ui/ui.php
+++ integration/ui/ui.php
@@ -9,7 +9,6 @@ $t = microtime(true) ;
 require_once dirname(__DIR__).'/jcat_init.php' ;
 UIFactory::singleton()->sourceFileManager()->addFolder(__DIR__.'/templates') ;
 
-
 $aUI = UIFactory::singleton()->create() ;
 $aUI->display("zengarden.template.html") ;
 
