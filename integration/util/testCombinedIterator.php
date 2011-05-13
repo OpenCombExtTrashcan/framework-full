5d4867f6fe2caf3751e67bb195026c4f5340050f
alee <alee@fb4ae8c7-ff60-4932-ad58-89b0e700feac>
2011-05-13 05:20:45 +0000

    git-svn-id: http://svn.jecat.cn/framework/0.6/trunk/test@2454 fb4ae8c7-ff60-4932-ad58-89b0e700feac

--- integration/util/testCombinedIterator.php
+++ integration/util/testCombinedIterator.php
@@ -6,16 +6,14 @@ use jc\util\CombinedIterator;
 require_once dirname(__DIR__).'/jcat_init.php' ;
 
 
-
+$empty = new \ArrayIterator(array()) ;
 
 $iter1 = new \ArrayIterator(array(1,2,3)) ;
 $iter2 = new \ArrayIterator(array('a','b','c','d')) ;
-$iter3 = new \ArrayIterator(array()) ;
-$iter4 = new \ArrayIterator(array('A','B','C','D','E')) ;
+$iter3 = new \ArrayIterator(array('A','B','C','D','E')) ;
 
-$citer = new CombinedIterator($iter1,$iter2,$iter3,$iter4) ;
 
-foreach ($citer as $v)
+foreach (new CombinedIterator($empty,$iter1,$iter2,$empty,$iter3,$empty,$iter2,$empty) as $v)
 {
 	echo $v, "\r\n" ;
 }
