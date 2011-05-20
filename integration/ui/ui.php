ee4136bf9fe79a48c3fc4462ad7e679d0373e86d
kongyuan <kongyuan@fb4ae8c7-ff60-4932-ad58-89b0e700feac>
2011-05-20 06:17:50 +0000

    git-svn-id: http://svn.jecat.cn/framework/0.6/trunk/test@2488 fb4ae8c7-ff60-4932-ad58-89b0e700feac

--- integration/ui/ui.php
+++ integration/ui/ui.php
@@ -1,6 +1,8 @@
 <?php 
 namespace jc\test\integration\ui ;
 
+use jc\lang\Object;
+
 use jc\ui\xhtml\Factory as UIFactory ;
 
 $t = microtime(true) ;
@@ -9,7 +11,7 @@ require_once dirname(__DIR__).'/jcat_init.php' ;
 UIFactory::singleton()->sourceFileManager()->addFolder(dirname(__DIR__).'/templates') ;
 
 $aUI = UIFactory::singleton()->create() ;
-$aUI->variables()->set('key','GFDAF') ;
+$aUI->variables()->set('GLOBAL',array(1,2,3,4,7,array(4,1,2,4,5))) ;
 $aUI->display("zengarden.template.html") ;
 
 echo microtime(true) - $t, "\r\n" ;
