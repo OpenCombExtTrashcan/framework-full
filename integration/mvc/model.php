cf32494a9599a605d9a1c6ff1a4b8b199eb7ab31
alee <alee@fb4ae8c7-ff60-4932-ad58-89b0e700feac>
2011-05-27 11:18:54 +0000

    git-svn-id: http://svn.jecat.cn/framework/0.6/trunk/test@2508 fb4ae8c7-ff60-4932-ad58-89b0e700feac

--- integration/mvc/model.php
+++ integration/mvc/model.php
@@ -10,51 +10,21 @@ use jc\mvc\model\db\orm\ModelPrototype;
 $aApp = require_once dirname(__DIR__).'/jcat_init.php' ;
 
 $aModel = new Model(array(
-		'name' => 'book' ,
-		'keys' => 'bid' ,
-		'table' => 'book' ,
+		'name' => 'feed' ,
+		'keys' => 'feedid' ,
+		'table' => 'myspace_feed' ,
 
 		'belongsTo' => array(
-
 			array(
-				'prop' => 'author' ,
-				'fromk' => 'author_uid' ,
+				'prop' => 'user' ,
+				'fromk' => 'uid' ,
 				'tok' => 'uid' ,
 				'model' => array(
 					'name' => 'user' ,
 					'keys' => 'uid' ,
-					'table' => 'user' ,
-			
-					'belongsTo' => array(
-					) ,
+					'table' => 'myspace_space' ,
 				) ,
 			) ,
-			
-			array(
-				'prop' => 'publishing' ,
-				'fromk' => 'publishing_cid' ,
-				'tok' => 'cid' ,
-				'model' => array(
-					'name' => 'company' ,
-					'keys' => 'cid' ,
-					'table' => 'company' ,
-			
-					
-					'hasOne' => array(
-						array(
-							'prop' => 'manager' ,
-							'fromk' => 'manager_uid' ,
-							'tok' => 'uid' ,
-							'model' => array(
-								'name' => 'user' ,
-								'keys' => 'uid' ,
-								'table' => 'user' ,
-							) ,
-						) ,
-					),
-				) ,
-			) ,
-			
 		) ,
 )) ;
 
@@ -63,51 +33,21 @@ $aModel->load() ;
 
 
 $aModel = new Model(array(
-		'name' => 'book' ,
-		'keys' => 'bid' ,
-		'table' => 'book' ,
+		'name' => 'feed' ,
+		'keys' => 'feedid' ,
+		'table' => 'myspace_feed' ,
 
 		'belongsTo' => array(
-
 			array(
-				'prop' => 'author' ,
-				'fromk' => 'author_uid' ,
+				'prop' => 'user' ,
+				'fromk' => 'uid' ,
 				'tok' => 'uid' ,
 				'model' => array(
 					'name' => 'user' ,
 					'keys' => 'uid' ,
-					'table' => 'user' ,
-			
-					'belongsTo' => array(
-					) ,
+					'table' => 'myspace_space' ,
 				) ,
 			) ,
-			
-			array(
-				'prop' => 'publishing' ,
-				'fromk' => 'publishing_cid' ,
-				'tok' => 'cid' ,
-				'model' => array(
-					'name' => 'company' ,
-					'keys' => 'cid' ,
-					'table' => 'company' ,
-			
-					
-					'hasOne' => array(
-						array(
-							'prop' => 'manager' ,
-							'fromk' => 'manager_uid' ,
-							'tok' => 'uid' ,
-							'model' => array(
-								'name' => 'user' ,
-								'keys' => 'uid' ,
-								'table' => 'user' ,
-							) ,
-						) ,
-					),
-				) ,
-			) ,
-			
 		) ,
 )) ;
 
