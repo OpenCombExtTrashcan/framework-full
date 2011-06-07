b07a91cbb2db0cc4122ee6a6783d2254ab7d7d55
alee <alee@fb4ae8c7-ff60-4932-ad58-89b0e700feac>
2011-06-07 11:21:23 +0000

    git-svn-id: http://svn.jecat.cn/framework/0.6/trunk/test@2569 fb4ae8c7-ff60-4932-ad58-89b0e700feac

--- integration/mvc/mvc.php
+++ integration/mvc/mvc.php
@@ -2,6 +2,8 @@
 namespace jc\test\integration\mvc ;
 
 // 
+use jc\verifier\Length;
+use jc\mvc\view\widget\Text;
 use jc\mvc\controller\Controller;
 use jc\mvc\view\View;
 use jc\ui\xhtml\Factory as UIFactory ;
@@ -10,36 +12,41 @@ $t = microtime(true) ;
 
 $aApp = require_once dirname(__DIR__).'/jcat_init.php' ;
 
-class MyChildController extends Controller
-{
-	protected function init()
-	{
-		$this->createView("childControllerView","simple-text.template.html") ;
-	}
-	
-	public function process()
-	{
-		$this->childControllerView->render() ;
-	}
-} 
-
 class MyController extends Controller
 {
 	protected function init()
 	{
-		$this->createView("view","simple-text.template.html") ;
-		
-		$this->add( new MyChildController() ) ;
+		$this->createView("view","simple-text.template.html",'jc\\mvc\\view\\FormView') ;
+		$this->view->addWidget(new Text('username'))->dataVerifiers()->add(
+			new Length(1, 2)
+		) ;
 	}
 	
 	public function process()
 	{
+		// print_r($this->aParams) ;
+		if( $this->view->isSubmit($this->aParams) )
+		{
+			$this->view->loadWidgets($this->aParams) ;
+			
+			if( !$this->view->verifyWidgets() )
+			{
+				
+			}
+			
+		}
+		
+		else 
+		{
+		}
+		
 		$this->view->render() ;
 	}
 } 
 
 $aController = new MyController() ;
-$aController->mainRun() ;
+$aController->mainRun( $aApp->request() ) ;
+
 
 echo microtime(true) - $t, "\r\n" ;
 echo (memory_get_peak_usage()/1024/1024), "mb\r\n" ;
