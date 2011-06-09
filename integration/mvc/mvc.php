4c9e6472b64a97196e40609761585c080fbedf15
kongyuan <kongyuan@fb4ae8c7-ff60-4932-ad58-89b0e700feac>
2011-06-09 07:36:09 +0000

    git-svn-id: http://svn.jecat.cn/framework/0.6/trunk/test@2578 fb4ae8c7-ff60-4932-ad58-89b0e700feac

--- integration/mvc/mvc.php
+++ integration/mvc/mvc.php
@@ -1,82 +1,48 @@
-<?php 
-namespace jc\test\integration\mvc ;
+<?php
+namespace jc\test\integration\mvc;
 
 // 
-use jc\mvc\view\widget\CheckBtn;
-
+use jc\mvc\view\widget\Select;
 use jc\verifier\Length;
 use jc\mvc\view\widget\Text;
 use jc\mvc\controller\Controller;
 use jc\mvc\view\View;
-use jc\ui\xhtml\Factory as UIFactory ;
+use jc\ui\xhtml\Factory as UIFactory;
 
-$t = microtime(true) ;
+$t = microtime ( true );
 
-$aApp = require_once dirname(__DIR__).'/jcat_init.php' ;
+$aApp = require_once dirname ( __DIR__ ) . '/jcat_init.php';
 
-class MyController extends Controller
-{
-	protected function init()
-	{
-		$this->createView("view","simple-text.template.html",'jc\\mvc\\view\\FormView') ;
+class MyController extends Controller {
+	protected function init() {
+		$this->createView ( "view", "simple-text.template.html", 'jc\\mvc\\view\\FormView' );
 		
-		$checkbox1 = new CheckBtn('radio1','编辑区域',CheckBtn::RADIO ,CheckBtn::CHEACKED);
-		$this->view->addWidget($checkbox1)->dataVerifiers()->add(
-			new Length(6, 20)
-		);
-		$checkbox1->setValueFromString("fdfsfds1");
-		$checkbox1->setFormName('name');
-		
-		$checkbox2 = new CheckBtn('radio2','编辑区域',CheckBtn::RADIO ,CheckBtn::CHEACKED);
-		$this->view->addWidget($checkbox2)->dataVerifiers()->add(
-			new Length(6, 20)
-		);
-		$checkbox2->setValueFromString("fdfsfds2");
-		$checkbox2->setFormName('name');
-		
-		$checkbox3 = new CheckBtn('radio3','编辑区域',CheckBtn::RADIO ,CheckBtn::CHEACKED);
-		$this->view->addWidget($checkbox3)->dataVerifiers()->add(
-			new Length(6, 20)
-		);
-		$checkbox3->setValueFromString("fdfsfds3");
-		$checkbox3->setFormName('name');
-		
-		$checkbox4 = new CheckBtn('radio4','编辑区域',CheckBtn::RADIO ,CheckBtn::CHEACKED);
-		$this->view->addWidget($checkbox4)->dataVerifiers()->add(
-			new Length(6, 20)
-		);
-		$checkbox4->setValueFromString("fdfsfds4");
-		$checkbox4->setFormName('name');
-		
-		$checkbox5 = new CheckBtn('radio5','编辑区域',CheckBtn::RADIO ,CheckBtn::CHEACKED);
-		$this->view->addWidget($checkbox5)->dataVerifiers()->add(
-			new Length(6, 20)
-		);
-		$checkbox5->setValueFromString("fdfsfds5");
-		$checkbox5->setFormName('name');
+		$select = new Select ( 'select', '选择国家' );
+		$this->view->addWidget ( $select )->dataVerifiers ()->add ( new Length ( 6, 20 ) );
+		$select->setValueFromString ( "selectValueTest" );
+		$select->addOption('a', 1);
+		$select->addOption('b', 2);
+		$select->addOption('c', 3);
+		$select->addOption('d', 4);
 	}
 	
-	public function process()
-	{
+	public function process() {
 		// print_r($this->aParams) ;
-		if( $this->view->isSubmit($this->aParams) )
-		{
-			$this->view->loadWidgets($this->aParams) ;
+		if ($this->view->isSubmit ( $this->aParams )) {
+			$this->view->loadWidgets ( $this->aParams );
 			
-			if( !$this->view->verifyWidgets() )
-			{
-				
+			if (! $this->view->verifyWidgets ()) {
+			
 			}
-			
+		
+		} 
+
+		else {
 		}
 		
-		else 
-		{
-		}
-		
-		$this->view->render() ;
+		$this->view->render ();
 	}
-} 
+}
 
 $aController = new MyController() ;
 $aController->mainRun( $aApp->request() ) ;
