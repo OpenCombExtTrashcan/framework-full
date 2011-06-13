5f21be157130d5f349d58c443a2bc5293efd523e
kongyuan <kongyuan@fb4ae8c7-ff60-4932-ad58-89b0e700feac>
2011-06-13 11:37:45 +0000

    git-svn-id: http://svn.jecat.cn/framework/0.6/trunk/test@2584 fb4ae8c7-ff60-4932-ad58-89b0e700feac

--- integration/mvc/mvc.php
+++ integration/mvc/mvc.php
@@ -8,6 +8,9 @@ use jc\mvc\model\db\Model;
 
 use jc\message\Message;
 use jc\verifier\Length;
+use jc\mvc\view\widget\CheckBtn;
+use jc\mvc\view\widget\Select;
+use jc\mvc\view\widget\Group;
 use jc\mvc\view\widget\Text;
 use jc\mvc\controller\Controller;
 use jc\mvc\view\View;
@@ -22,49 +25,89 @@ class MyController extends Controller
 	protected function init()
 	{
 		$this->createView("view","simple-text.template.html",'jc\\mvc\\view\\FormView') ;
-		$this->view->addWidget(new Text('username',"用户名"))->dataVerifiers()->add(
-			new Length(1, 2)
-		) ;
-		$this->view->dataExchanger()->link('username','username') ;
+		
+		$radio = new CheckBtn('radio' , 'radiotest' , CheckBtn::RADIO);
+		$this->view->addWidget ( $radio );
+		$radio->setValue('radio');
+		
+		$checkbox = new CheckBtn('checkbox' , 'radiotest' , CheckBtn::CHECKBOX);
+		$this->view->addWidget ( $checkbox );
+		$checkbox->setValue('checkboxValue');
+		
+		$select = new Select ( 'select', '选择国家' );
+		$this->view->addWidget ( $select );
+		$select->addOption('a', 1);
+		$select->addOption('b', 2);
+		$select->addOption('c', 3);
+		$select->addOption('d', 4);
+		$select->setSize(4);
+		$select->setValueFromString ( "selectValueTest" );
+		
+		$group = new Group('group' , 'testGroup');
+		$this->view->addWidget ( $group );
+		$group->addWidget($radio);
+		$group->addWidget($checkbox);
+		$group->addWidget($select);
+		
+		$checkbox2 = new CheckBtn('checkbox2' , 'radiotest' , CheckBtn::CHECKBOX);
+		$this->view->addWidget ( $checkbox2 );
+		
+		$group2 = new Group('group2' , 'testGroup');
+		$this->view->addWidget ( $group2 );
+		$group2->addWidget($checkbox2);
+		$group2->addWidget($group);
+		$values = $group2->valueToString();
+		$group2->setValueFromString($values);
+		var_dump($group->valueToString());
+		exit();
+		
+		
+//		var_dump($group2->valueToString());
+//		exit();
+		
+//		
+//		$this->aModel = new Model(array(
+//				'name' => 'feed' ,
+//				'keys' => 'feedid' ,
+//				'table' => 'myspace_feed' ,
+//		
+//				'belongsTo' => array(
+//					array(
+//						'prop' => 'user' ,
+//						'fromk' => 'uid' ,
+//						'tok' => 'uid' ,
+//						'model' => array(
+//							'name' => 'user' ,
+//							'keys' => 'uid' ,
+//							'table' => 'myspace_space' ,
+//						) ,
+//					) ,
+//				) ,
+//				
+//				'hasAndBelongsMany' => array(
+//					array(
+//						'prop' => 'buffs' ,
+//						'fromk' => 'uid' ,
+//						'btok' => 'uid' ,	
+//						'bfromk' => 'buffid' ,
+//						'tok' => 'id' ,
+//						'bridge' => 'myspace_mbuff' ,
+//					
+//						'model' => array(
+//							'name' => 'buff' ,
+//							'keys' => 'id' ,
+//							'table' => 'myspace_buff' ,
+//						) ,
+//					)
+//				),
+//		),true) ;
+//		$this->view->setModel($this->aModel) ;
 		
 		
 		
-		$this->aModel = new Model(array(
-				'name' => 'feed' ,
-				'keys' => 'feedid' ,
-				'table' => 'myspace_feed' ,
 		
-				'belongsTo' => array(
-					array(
-						'prop' => 'user' ,
-						'fromk' => 'uid' ,
-						'tok' => 'uid' ,
-						'model' => array(
-							'name' => 'user' ,
-							'keys' => 'uid' ,
-							'table' => 'myspace_space' ,
-						) ,
-					) ,
-				) ,
-				
-				'hasAndBelongsMany' => array(
-					array(
-						'prop' => 'buffs' ,
-						'fromk' => 'uid' ,
-						'btok' => 'uid' ,	
-						'bfromk' => 'buffid' ,
-						'tok' => 'id' ,
-						'bridge' => 'myspace_mbuff' ,
-					
-						'model' => array(
-							'name' => 'buff' ,
-							'keys' => 'id' ,
-							'table' => 'myspace_buff' ,
-						) ,
-					)
-				),
-		),true) ;
-		$this->view->setModel($this->aModel) ;
+		
+		
 	}
 	
 	public function process()
