2df1c56dd3598c9d7b4949c52a875980ba5944ab
kongyuan <kongyuan@fb4ae8c7-ff60-4932-ad58-89b0e700feac>
2011-06-16 08:30:02 +0000

    git-svn-id: http://svn.jecat.cn/framework/0.6/trunk/test@2601 fb4ae8c7-ff60-4932-ad58-89b0e700feac

---
+++ integration/mvc/widget.php
@@ -0,0 +1,113 @@
+<?php 
+namespace jc\test\integration\widget ;
+
+use jc\mvc\view\DataExchanger;
+use jc\mvc\model\db\Model;
+use jc\message\Message;
+use jc\verifier\Length;
+use jc\verifier\ConsistenceEqualCheck;
+use jc\verifier\Email;
+use jc\mvc\view\widget\CheckBtn;
+use jc\mvc\view\widget\Select;
+use jc\mvc\view\widget\Group;
+use jc\mvc\view\widget\RadioGroup;
+use jc\mvc\view\widget\Text;
+use jc\mvc\controller\Controller;
+use jc\mvc\view\View;
+use jc\ui\xhtml\Factory as UIFactory ;
+
+$t = microtime(true) ;
+
+$aApp = require_once dirname(__DIR__).'/jcat_init.php' ;
+
+class WidgetController extends Controller
+{
+	protected function init()
+	{
+		$this->createView("widgetTestView","widget-test.template.html",'jc\\mvc\\view\\FormView') ;
+		
+		///测试group代码 , password 核对
+		$username = new Text('username' , '用户名' ,Text::TEXT);
+		$this->widgetTestView->addWidget ( $username )->dataVerifiers()->add(
+			Length::flyweight(2 , 3)
+		);
+		$email = new Text('email' , '邮件' ,Text::TEXT);
+		$this->widgetTestView->addWidget ( $email )->dataVerifiers()->add(
+			Length::flyweight(6 , 13)
+		);
+		$password1 = new Text('password1' , '确认密码1' ,Text::PASSWORD);
+		$this->widgetTestView->addWidget ( $password1 )->dataVerifiers()->add(
+			Length::flyweight(6,8)
+		);
+		$password2 = new Text('password2' , '确认密码2' ,Text::PASSWORD);
+		$this->widgetTestView->addWidget ( $password2 )->dataVerifiers()->add(
+			Length::flyweight(6,8)
+		);
+		$group = new Group('passwordGroup' , '密码核对');
+		$group->addWidget($password1);
+		$group->addWidget($password2);
+		$this->widgetTestView->addWidget ( $group )->dataVerifiers()->add(
+			ConsistenceEqualCheck::singleton()
+		);
+		
+		//测试radiogroup
+//		$radio1 = new CheckBtn('radio1' , 'radiotest1' ,  CheckBtn::RADIO , 'radio1');
+//		$this->widgetTestView->addWidget ( $radio1 );
+//		
+//		$radio2 = new CheckBtn('radio2' , 'radiotest2' , CheckBtn::RADIO , 'radio2');
+//		$this->widgetTestView->addWidget ( $radio2 );
+//		$radio2->setChecked();
+//		
+//		$radio3 = new CheckBtn('radio3' , 'radiotest3' , CheckBtn::RADIO ,'radio3');
+//		$this->widgetTestView->addWidget ( $radio3 );
+//		
+//		$radio4 = new CheckBtn('radio4' , 'radiotest4' , CheckBtn::RADIO ,'radio4');
+//		$this->widgetTestView->addWidget ( $radio4 );
+//		
+//		$group = new RadioGroup('radioGroup' , 'testGroup');
+//		$this->widgetTestView->addWidget ( $group );
+//		$group->addWidget($radio1);
+//		$group->addWidget($radio2);
+//		$group->addWidget($radio3);
+//		$group->addWidget($radio4);
+//		$group->setChecked('radio3');
+//		echo $group->valueToString();
+
+		
+	}
+	
+public function process()
+	{
+		if( $this->widgetTestView->isSubmit($this->aParams) )
+		{
+			do{
+				$this->widgetTestView->loadWidgets($this->aParams) ;
+				
+				if( !$this->widgetTestView->verifyWidgets() )
+				{
+					$aMsgQueue = $this->messageQueue() ;
+					$this->widgetTestView->render() ;
+					return ;
+				}
+			
+				$this->widgetTestView->exchangeData(DataExchanger::WIDGET_TO_MODEL) ;
+				
+				new Message(Message::success,"表单提交完成") ;
+				
+			} while(0) ;
+		}
+		
+		else 
+		{
+			$this->widgetTestView->render() ;
+		}
+
+	}
+} 
+
+$aController = new WidgetController() ;
+$aController->mainRun( $aApp->request() ) ;
+echo microtime(true) - $t, "\r\n" ;
+echo (memory_get_peak_usage()/1024/1024), "mb\r\n" ;
+
+?>
