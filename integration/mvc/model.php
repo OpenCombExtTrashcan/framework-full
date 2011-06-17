27dcd4674cc4a824f5d03597e1339f94f5bb52cd
kongyuan <kongyuan@fb4ae8c7-ff60-4932-ad58-89b0e700feac>
2011-06-17 03:53:44 +0000

    git-svn-id: http://svn.jecat.cn/framework/0.6/trunk/test@2616 fb4ae8c7-ff60-4932-ad58-89b0e700feac

--- integration/mvc/model.php
+++ integration/mvc/model.php
@@ -17,12 +17,63 @@ echo "init framework: ", microtime(true) - $t, "<br />\r\n" ;
 $t = microtime(true) ;
 
 
-$aModel = new Model(array(
-		'name' => 'feed' ,
-		'keys' => 'feedid' ,
-		'table' => 'myspace_feed' ,
+$aUser = new Model(
+	array(
+		'name' => 'user' ,
+		'keys' => 'uid' ,
+		'table' => 'user' ,
+		'hasOne' => array(
+			array(
+				'prop' => 'info' ,
+				'fromk' => 'uid' ,
+				'tok' => 'uid' ,
+			
+				'model' => array(
+					'name' => 'userinfo' ,
+					'keys' => 'uid' ,
+					'table' => 'userinfo' ,
+				)
+			)
+		) ,
+		'hasAndBelongsToMany' => array(
+			array(
+				'prop' => 'friends' ,
+				'fromk' => 'uid' ,
+				'tok' => 'uid' ,
+				'bfromk' => 'uid' ,
+				'btok' => 'fuid' ,	
+				'bridge' => 'userfriends' ,
+			
+				'model' => array(
+					'name' => 'user' ,
+					'keys' => 'uid' ,
+					'table' => 'user' ,
+				)
+			) ,
+			array(
+				'prop' => 'author' ,
+				'fromk' => 'uid' ,
+				'tok' => 'uid' ,
+				'btok' => 'eid' ,	
+				'bfromk' => 'eid' ,
+				'bridge' => 'epubauthor' ,
+			
+				'model' => array(
+					'name' => 'equb' ,
+					'keys' => 'eid' ,
+					'table' => 'equb' ,
+				)
+			)
+		),
+	)
+,true) ;
 
-		'belongsTo' => array(
+$aUserinfo = new Model(
+	array(
+		'name' => 'userinfo' ,
+		'keys' => 'uid' ,
+		'table' => 'userinfo' ,
+		'hasOne' => array(
 			array(
 				'prop' => 'user' ,
 				'fromk' => 'uid' ,
@@ -30,28 +81,132 @@ $aModel = new Model(array(
 				'model' => array(
 					'name' => 'user' ,
 					'keys' => 'uid' ,
-					'table' => 'myspace_space' ,
-				) ,
-			) ,
-		) ,
-		
-		'hasAndBelongsMany' => array(
-			array(
-				'prop' => 'buffs' ,
-				'fromk' => 'uid' ,
-				'btok' => 'uid' ,	
-				'bfromk' => 'buffid' ,
-				'tok' => 'id' ,
-				'bridge' => 'myspace_mbuff' ,
-			
-				'model' => array(
-					'name' => 'buff' ,
-					'keys' => 'id' ,
-					'table' => 'myspace_buff' ,
-				) ,
-			)
+					'table' => 'user' ,
+				),
+			),
 		),
-),true) ;
+	)
+,true) ;
+
+
+$aUserfriend = new Model(
+	array(
+		'name' => 'userfriend' ,
+		'keys' => 'ufid' ,
+		'table' => 'serfriend' ,
+		'hasOne' => array(
+			array(
+				'prop' => 'user' ,
+				'fromk' => 'uid' ,
+				'tok' => 'uid' ,
+				'model' => array(
+					'name' => 'user' ,
+					'keys' => 'uid' ,
+					'table' => 'user' ,
+				),
+			),
+			array(
+				'prop' => 'friend' ,
+				'fromk' => 'fuid' ,
+				'tok' => 'uid' ,
+				'model' => array(
+					'name' => 'user' ,
+					'keys' => 'uid' ,
+					'table' => 'user' ,
+				),
+			),
+		),
+	)
+,true) ;
+
+$aEpub = new Model(
+	array(
+		'name' => 'epub' ,
+		'keys' => 'eid' ,
+		'table' => 'epub' ,
+		'hasOne' => array(
+			array(
+				'prop' => 'author' ,
+				'fromk' => 'eid' ,
+				'tok' => 'eid' ,
+				'model' => array(
+					'name' => 'epubauthor' ,
+					'keys' => 'eid' ,
+					'table' => 'epubauthor' ,
+				),
+			),
+			array(
+				'prop' => 'categories' ,
+				'fromk' => 'cid' ,
+				'tok' => 'cid' ,
+				'model' => array(
+					'name' => 'epubcategories' ,
+					'keys' => 'cid' ,
+					'table' => 'epubcategories' ,
+				),
+			),
+		),
+	)
+,true) ;
+
+$aEpubauthor = new Model(
+	array(
+		'name' => 'epubauthor' ,
+		'keys' => 'aid' ,
+		'table' => 'epubauthor' ,
+		'hasOne' => array(
+			array(
+				'prop' => 'user' ,
+				'fromk' => 'uid' ,
+				'tok' => 'uid' ,
+				'model' => array(
+					'name' => 'user' ,
+					'keys' => 'uid' ,
+					'table' => 'user' ,
+				),
+			),
+			array(
+				'prop' => 'epub' ,
+				'fromk' => 'eid' ,
+				'tok' => 'eid' ,
+				'model' => array(
+					'name' => 'epub' ,
+					'keys' => 'eid' ,
+					'table' => 'epub' ,
+				),
+			),
+		),
+	)
+,true) ;
+
+$aEpubcategories = new Model(
+	array(
+		'name' => 'epubcategories' ,
+		'keys' => 'cid' ,
+		'table' => 'epubcategories' ,
+		'hasOne' => array(
+			array(
+				'prop' => 'epub' ,
+				'fromk' => 'cid' ,
+				'tok' => 'cid' ,
+				'model' => array(
+					'name' => 'epub' ,
+					'keys' => 'eid' ,
+					'table' => 'epub' ,
+				),
+			),
+		),
+	)
+,true) ;
+
+$aUser->data('username') ;
+$aUser->username ;
+$aUser['username'] ;
+
+
+$aUser->data('userinfo.email') ;
+$aUser['userinfo.xnenene.xssss.email'] ;
+
 
 $aModel->delete() ;
 
@@ -77,7 +232,7 @@ $aModel = new Model(array(
 			) ,
 		) ,
 		
-		'hasAndBelongsMany' => array(
+		'hasAndBelongsToMany' => array(
 			array(
 				'prop' => 'buffs' ,
 				'fromk' => 'uid' ,
@@ -92,7 +247,7 @@ $aModel = new Model(array(
 					'table' => 'myspace_buff' ,
 				) ,
 			)
-		),
+		)
 ),true) ;
 
 
