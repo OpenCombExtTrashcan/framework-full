d8fa175db6603c06ac746e58512c088dad64ee0b
kongyuan <kongyuan@fb4ae8c7-ff60-4932-ad58-89b0e700feac>
2011-06-22 08:34:41 +0000

    git-svn-id: http://svn.jecat.cn/framework/0.6/trunk/test@2665 fb4ae8c7-ff60-4932-ad58-89b0e700feac

--- integration/mvc/widget.php
+++ integration/mvc/widget.php
@@ -7,7 +7,8 @@ use jc\message\Message;
 use jc\verifier\Length;
 use jc\verifier\Same;
 use jc\verifier\Email;
-use jc\verifier\NotNull;
+use jc\verifier\NotEmpty;
+use jc\verifier\Number;
 use jc\mvc\view\widget\CheckBtn;
 use jc\mvc\view\widget\Select;
 use jc\mvc\view\widget\SelectList;
@@ -28,48 +29,50 @@ class WidgetController extends Controller {
 		$this->createView ( "widgetTestView", "widget-test.template.html", 'jc\\mvc\\view\\FormView' );
 		
 		///测试group代码 , 模拟用户注册
-		$username = new Text ( 'username', '用户名', Text::TEXT );
-		$this->widgetTestView->addWidget ( $username )->dataVerifiers ()->add ( Length::flyweight ( 2, 3 ) );
-		$email = new Text ( 'email', '邮件', Text::TEXT );
-		$this->widgetTestView->addWidget ( $email )->dataVerifiers ()->add ( Length::flyweight ( 6, 13 ) )->add ( Email::singleton () );
-		$password1 = new Text ( 'password1', '确认密码1', Text::PASSWORD );
-		$this->widgetTestView->addWidget ( $password1 )->dataVerifiers ()->add ( Length::flyweight ( 6, 8 ) );
-		$password2 = new Text ( 'password2', '确认密码2', Text::PASSWORD );
-		$this->widgetTestView->addWidget ( $password2 )->dataVerifiers ()->add ( Length::flyweight ( 6, 8 ) );
-		$group = new Group ( 'passwordGroup', '密码核对' );
-		$group->addWidget ( $password1 );
-		$group->addWidget ( $password2 );
-		$this->widgetTestView->addWidget ( $group )->dataVerifiers ()->add ( Same::singleton (), "两次输入的密码不相等" );
+		$username = new Text('username');
+		$this->widgetTestView->addWidget ( $username )->dataVerifiers ()->add ( Length::flyweight ( 2, 5 ) )->add ( Number::flyweight ( false ) );
+//		$email = new Text ( 'email', '邮件', Text::TEXT );
+//		$this->widgetTestView->addWidget ( $email )->dataVerifiers ()->add ( Length::flyweight ( 6, 13 ) )->add ( Email::singleton () );
+//		$password1 = new Text ( 'password1', '确认密码1', Text::PASSWORD );
+//		$this->widgetTestView->addWidget ( $password1 )->dataVerifiers ()->add ( Length::flyweight ( 6, 8 ) );
+//		$password2 = new Text ( 'password2', '确认密码2', Text::PASSWORD );
+//		$this->widgetTestView->addWidget ( $password2 )->dataVerifiers ()->add ( Length::flyweight ( 6, 8 ) );
+//		$group = new Group ( 'passwordGroup', '密码核对' );
+//		$group->addWidget ( $password1 );
+//		$group->addWidget ( $password2 );
+//		$this->widgetTestView->addWidget ( $group )->dataVerifiers ()->add ( Same::singleton (), "两次输入的密码不相等" );
+//		
+//		$select = new Select ( 'select', '选择省', 1 );
+//		$select->addOption ( null, "请选择...", true );
+//		$select->addOption ( "fda", "342" );
+//		$select->addOption ( "asf", "fdsa" );
+//		$select->addOption ( "fda", "543" );
+//		$select->addOption ( "fdffa", "fdsa" );
+//		$select->addOption ( "selectvalue", "selectvalue" );
+//		$select->addOption ( "5434", "fvfvasf" );
+//		$select->addOption ( "fdasf", "fda" );
+//		$this->widgetTestView->addWidget ( $select )->dataVerifiers ()->add ( NotEmpty::singleton (), "请选择省" );
+//		
+//		$selectlist = new SelectList ( 'selectlist', '选择城市', 6 );
+//		$selectlist->addOption ( "fda1", "342" );
+//		$selectlist->addOption ( "asf2", "fdsa" );
+//		$selectlist->addOption ( "selectlistvalue3", "selectlistvalue");
+//		$selectlist->addOption ( "fda4", "543" );
+//		$selectlist->addOption ( "fda5", "fdsa" );
+//		$selectlist->addOption ( "543a6", "fvfvasf" );
+//		$selectlist->addOption ( "fdafadsf7", "fda" );
+//		$this->widgetTestView->addWidget ( $selectlist )->dataVerifiers ()->add ( NotEmpty::singleton (), "请选择城市" );
+//		
+//		$checkbox1 = new CheckBtn ( 'checkbox1', '电影', CheckBtn::CHECKBOX, 'movie' );
+//		$this->widgetTestView->addWidget ( $checkbox1 );
+//		
+		$radioGroup = new RadioGroup('radiogroup','选择喜欢的球类');
+		$radioGroup->createRadio('checkbox1', "篮球", "basketball")->createRadio('checkbox2', "足球", "football")->createRadio('checkbox3', "网球", "tennis");
+		$this->widgetTestView->addWidget($radioGroup);
 		
-		$select = new Select ( 'select', '选择省', 1 );
-		$select->addOption ( null, "请选择...", true );
-		$select->addOption ( "fda", "342" );
-		$select->addOption ( "asf", "fdsa" );
-		$select->addOption ( "fda", "543" );
-		$select->addOption ( "fdffa", "fdsa" );
-		$select->addOption ( "selectvalue", "selectvalue" );
-		$select->addOption ( "5434", "fvfvasf" );
-		$select->addOption ( "fdasf", "fda" );
-		$this->widgetTestView->addWidget ( $select )->dataVerifiers ()->add ( NotNull::singleton (), "请选择省" );
-		
-		$selectlist = new SelectList ( 'selectlist', '选择城市', 6 );
-		$selectlist->addOption ( "fda1", "342" );
-		$selectlist->addOption ( "asf2", "fdsa" );
-		$selectlist->addOption ( "selectlistvalue3", "selectlistvalue");
-		$selectlist->addOption ( "fda4", "543" );
-		$selectlist->addOption ( "fda5", "fdsa" );
-		$selectlist->addOption ( "543a6", "fvfvasf" );
-		$selectlist->addOption ( "fdafadsf7", "fda" );
-		$this->widgetTestView->addWidget ( $selectlist )->dataVerifiers ()->add ( NotNull::singleton (), "请选择城市" );
-		
-		$checkbox1 = new CheckBtn ( 'checkbox1', '电影', CheckBtn::CHECKBOX, 'movie' );
-		$this->widgetTestView->addWidget ( $checkbox1 );
-		
-		$checkbox2 = new CheckBtn ( 'checkbox2', '足球', CheckBtn::CHECKBOX, 'football' );
-		$this->widgetTestView->addWidget ( $checkbox2 );
-		
-		$fileupdate = new FileUpdate ( 'fileupdate', '文件上传' );
-		$this->widgetTestView->addWidget ( $fileupdate );
+//		
+//		$fileupdate = new FileUpdate ( 'fileupdate', '文件上传' );
+//		$this->widgetTestView->addWidget ( $fileupdate );
 	}
 	
 	public function process() {
