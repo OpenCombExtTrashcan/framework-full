c8d4297c5b12c8c2a5db40fd4e842e48013896d8
zq@alee2002.com <myname@server>
2011-08-11 09:54:26 +0800

    将 pear 放到 test/unit 内

--- unit/init.inc.php
+++ unit/init.inc.php
@@ -1,17 +1,17 @@
 <?php 
 namespace jc\test\unit ;
 
-use jc\system\AppFactory;
+use jc\system\ApplicationFactory ;
 
 // 添加 pear 各模组所需要的 include path
-define('jc\\test\\unit\\PATH_JC_ROOT',dirname(dirname(__DIR__))) ;
-define('jc\\test\\unit\\PATH_PEAR_ROOT',PATH_JC_ROOT.'/pear') ;
+define('jc\\test\\unit\\PATH_JC_ROOT',dirname(dirname(__DIR__)).'/framework') ;
+define('jc\\test\\unit\\PATH_PEAR_ROOT',__DIR__.'/pear') ;
 define('jc\\test\\unit\\PATH_PEAR_LIB',PATH_PEAR_ROOT.'/share/pear') ;
 
 set_include_path( get_include_path() . PATH_SEPARATOR . PATH_PEAR_LIB ) ;
 
 // 初始化 jecat 框架
-include PATH_JC_ROOT."/framework/inc.entrance.php" ;
+include PATH_JC_ROOT."/inc.entrance.php" ;
 
-return AppFactory::createFactory()->create(__DIR__) ; 
+return ApplicationFactory::singleton()->create(__DIR__) ; 
 ?>
