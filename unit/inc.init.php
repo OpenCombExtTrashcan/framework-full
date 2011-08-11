2d1678f973ad0ebe4482ce4e4b4d748aeb476bcc
lexdene <lexdene@sohu.com>
2011-08-11 15:32:14 +0800

    why 0 is equal to a string

--- unit/inc.init.php
+++ unit/inc.init.php
@@ -7,6 +7,7 @@ use jc\system\ApplicationFactory ;
 // 添加 pear 各模组所需要的 include path
 define('jc\\test\\unit\\PATH_JC_ROOT',dirname(dirname(__DIR__)).'/framework') ;
 define('jc\\test\\unit\\PATH_PEAR_ROOT',__DIR__.'/pear') ;
+define('jc\\test\\unit\\PATH_DATA_ROOT',__DIR__.'/data') ;
 define('jc\\test\\unit\\PATH_PEAR_LIB',PATH_PEAR_ROOT.'/share/pear') ;
 
 set_include_path( PATH_PEAR_LIB . PATH_SEPARATOR . get_include_path() ) ;
