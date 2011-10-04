bd35c9f910813c51f963b7167674bb8ccf370658
kongyuan <anubiskong@gmail.com>
2011-10-04 13:35:14 +0800

    ...

--- test/unit/case/jc/setting/SettingTest.php
+++ test/unit/case/jc/setting/SettingTest.php
@@ -29,19 +29,6 @@ class SettingTest extends \PHPUnit_Framework_TestCase
 	
 	public function testKeyIterator()
 	{
-		$aSetting = new SettingClassForTest ();
-		
-		$aSetting->createKey ( 'testPath/testPath1' );
-		$aSetting->createKey ( 'testPath/testPath2' );
-		$aSetting->createKey ( 'testPath/testPath3' );
-		//获得迭代器
-		$aKeyIter = $aSetting->keyIterator ( 'testPath' );
-		
-		$this->assertEquals ( 'testPath1', $aKeyIter->current () );
-		$aKeyIter->next ();
-		$this->assertEquals ( 'testPath2', $aKeyIter->current () );
-		$aKeyIter->next ();
-		$this->assertEquals ( 'testPath3', $aKeyIter->current () );
 	}
 	
 	public function testItemIterator()
@@ -51,26 +38,82 @@ class SettingTest extends \PHPUnit_Framework_TestCase
 		$aSetting->setItem ( 'testPath', 'testName1', 'testValue1' );
 		$aSetting->setItem ( 'testPath', 'testName2', 1 );
 		$aSetting->setItem ( 'testPath', 'testName3', false );
+		
+		//获得迭代器
+		$aIter = $aSetting->itemIterator('testPath');
+		
+		$this->assertEquals ( 'testName1', $aIter->current () );
+		$aIter->next ();
+		$this->assertEquals ( 'testName2', $aIter->current () );
+		$aIter->next ();
+		$this->assertEquals ( 'testName3', $aIter->current () );
 	}
 	
 	public function testItem()
 	{
-	
+		$aSetting = new SettingClassForTest ();
+		
+		$aSetting->setItem ( 'testPath', 'testName1', 'testValue1' );
+		$aSetting->setItem ( 'testPath', 'testName2', 1 );
+		$aSetting->setItem ( 'testPath', 'testName3', false );
+		
+		//取已存在的想的值
+		$this->assertEquals( 'testValue1', $aSetting->item('testPath','testName1' , 'defaultValue'));
+		$this->assertEquals( 1, $aSetting->item('testPath','testName2'));
+		$this->assertEquals( false, $aSetting->item('testPath','testName3'));
+		
+		//取不存在的值
+		$this->assertNull($aSetting->item('testPath','testName4'));
+		
+		//取不存在的值,带默认值
+		$this->assertEquals('defaultValue' , $aSetting->item('testPath','testName4','defaultValue'));
+		
+		//带默认值取不存在的值以后,那个值就存在了
+		$this->assertEquals('defaultValue', $aSetting->item('testPath','testName4'));
 	}
 	
+	/**
+	 * @see testItem()
+	 */
 	public function testSetItem()
 	{
-	
+		$this->testItem();
 	}
 	
 	public function testHasItem()
 	{
-	
+		$aSetting = new SettingClassForTest ();
+		
+		$aSetting->setItem ( 'testPath', 'testName1', 'testValue1' );
+		$aSetting->setItem ( 'testPath', 'testName2', 1 );
+		$aSetting->setItem ( 'testPath', 'testName3', false );
+		
+		//存在项
+		$this->assertTrue($aSetting->hasItem('testPath', 'testName1'));
+		$this->assertTrue($aSetting->hasItem('testPath', 'testName2'));
+		$this->assertTrue($aSetting->hasItem('testPath', 'testName3'));
+		
+		//不存在的项
+		$this->assertFalse($aSetting->hasItem('testPath', 'testName4'));
 	}
 	
 	public function testDeleteItem()
 	{
-	
+		$aSetting = new SettingClassForTest ();
+		
+		$aSetting->setItem ( 'testPath', 'testName1', 'testValue1' );
+		$aSetting->setItem ( 'testPath', 'testName2', 1 );
+		$aSetting->setItem ( 'testPath', 'testName3', false );
+		
+		//删除项
+		$aSetting->deleteItem('testPath', 'testName1');
+		$aSetting->deleteItem('testPath', 'testName2');
+		$aSetting->deleteItem('testPath', 'testName3');
+		
+		//不存在的项
+		$this->assertFalse($aSetting->hasItem('testPath', 'testName1'));
+		$this->assertFalse($aSetting->hasItem('testPath', 'testName2'));
+		$this->assertFalse($aSetting->hasItem('testPath', 'testName3'));
 	}
 }
 ?>
